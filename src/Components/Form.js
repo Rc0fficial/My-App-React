c04f7e72912f30410d1a0e8485a08319e1f8d5a7
Arsalan Ali Khattak <thunder9905@gmail.com>
2022-09-13 00:20:30 +0500

    upper case logic added

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,15 +1,34 @@
 import Form from "react-bootstrap/Form";
+import React, {useState}  from "react";
+
 
 function TextControlsExample(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState('Hello');
+
   return (
     <div className="container">
       <h1>{props.heading}</h1>
       <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group
+          className="mb-3"
+          value={text}
+          onChange={handleOnChange}
+          controlId="exampleForm.ControlTextarea1"
+        >
           <Form.Control as="textarea" rows={10} />
         </Form.Group>
       </Form>
-      <button className="btn btn-primary">Convert to upper case</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to upper case
+      </button>
     </div>
   );
 }
