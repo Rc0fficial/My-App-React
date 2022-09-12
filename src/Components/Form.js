51c42d4f38557b2c6b7379abb1f4627ffea61df7
Arsalan Ali Khattak <thunder9905@gmail.com>
2022-09-12 22:50:26 +0500

    text area updated to myBox

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,12 +3,9 @@ import Form from 'react-bootstrap/Form';
 function TextControlsExample() {
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
+
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
+        <Form.Label>myBox</Form.Label>
         <Form.Control as="textarea" rows={3} />
       </Form.Group>
     </Form>
