43a732ab3d172aa60728b470a1446db914538d32
Arsalan Ali Khattak <thunder9905@gmail.com>
2022-09-12 23:04:57 +0500

    final form of Form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,11 +3,10 @@ import Form from "react-bootstrap/Form";
 function TextControlsExample(props) {
   return (
     <div className="container">
-        <h1>{props.heading}</h1>
+      <h1>{props.heading}</h1>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label> {props.heading} </Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" rows={10} />
         </Form.Group>
       </Form>
     </div>
