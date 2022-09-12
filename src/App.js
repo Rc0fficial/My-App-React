68fc54ce1da3a8362ef579731d80cc5e65db45a1
Arsalan Ali Khattak <thunder9905@gmail.com>
2022-09-12 22:44:05 +0500

    form added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./Components/Navbar";
+import Form from "./Components/Form";
+
 
 function App() {
   return (
     <>
       <Navbar title = "Arsol" about = "about" />
+      <Form/>
+      
     </>
   );
 }
