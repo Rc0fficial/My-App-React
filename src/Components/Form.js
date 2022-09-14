00f0bcf60dd2d4e4920cad1591594e2947b69256
Arsalan Ali Khattak <thunder9905@gmail.com>
2022-09-14 10:26:33 +0500

    clear button added

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -11,6 +11,10 @@ function TextControlsExample(props) {
     let newText = text.toLowerCase();
     setText(newText);
   };
+  const handleClearClick = () => {
+    let newText = ('');
+    setText(newText);
+  };
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
@@ -19,34 +23,38 @@ function TextControlsExample(props) {
 
   return (
     <>
-     <div className="container">
-      <h1>{props.heading}</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Control
-            as="textarea"
-            value={text}
-            onChange={handleOnChange}
-            rows={10}
-          />
-        </Form.Group>
-      </Form>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to upper case
-      </button>
-      <button className="btn btn-primary mx-4" onClick={handleLowClick}>
-        Convert to Lower case
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Control
+              as="textarea"
+              value={text}
+              onChange={handleOnChange}
+              rows={10}
+            />
+          </Form.Group>
+        </Form>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to upper case
         </button>
-    </div>
-    <div className="container my-5">
+        <button className="btn btn-primary mx-4" onClick={handleLowClick}>
+          Convert to Lower case
+        </button>
+        <button className="btn btn-primary " onClick={handleClearClick}>
+          Clear text
+        </button>
+      </div>
+      <div className="container my-5">
         <h1>Your text summary</h1>
-        <p>{text.split(" ").length} and {text.length} characters </p>
+        <p>
+          {text.split(" ").length} and {text.length} characters{" "}
+        </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text}</p>
-    </div>
+      </div>
     </>
-   
   );
 }
 
