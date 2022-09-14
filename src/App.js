da0fe54479bc1341b69445e00c99f773c3eeb55d
Arsalan Ali Khattak <thunder9905@gmail.com>
2022-09-15 01:01:25 +0500

    dark mode added on about page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./Components/Navbar";
-import Form from "./Components/Form";
+// import Form from "./Components/Form";
+import About from "./Components/About";
+
 
 
 function App() {
   return (
     <>
       <Navbar title = "Arsol" about = "about" />
-      <Form heading = "Type your text inside" />
+      {/* <Form heading = "Type your text inside" /> */}
+      {/* <About /> */}
+      <About/>
       
     </>
   );
